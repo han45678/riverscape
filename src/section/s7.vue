92b714a94464157f203920538264138879612eff
han <han@deaf.com.tw>
2026-01-02 15:03:14 +0800

    s7

--- src/section/s7.vue
+++ src/section/s7.vue
@@ -8,14 +8,17 @@ import { ref } from 'vue';
     id="s7"
   >
     <div class="text">
-      <h2>
-        極致選品
-        <span>為生活注入國際規格的講究</span>
-      </h2>
-      <p>
-        24項全球精選品牌，從裡到外皆是藝術<br />
-        在鼎吉水岸，建材不是選配，而是一種品味的基本標準。
-      </p>
+      <div class="text_content">
+        <h2>
+          極致選品
+          <span>為生活注入國際規格的講究</span>
+        </h2>
+        <p>
+          24項全球精選品牌，從裡到外皆是藝術<br />
+          在鼎吉水岸，建材不是選配，而是一種品味的基本標準。
+        </p>
+      </div>
+      <div class="materials">Architecture Materials</div>
     </div>
     <div class="logo"></div>
   </section>
@@ -41,27 +44,49 @@ $color-text-white: #ffffff;
     padding-left: size(140);
     padding-bottom: size(140);
     display: flex;
-    gap: size(125);
   }
 
   .text {
     width: 30%;
     color: #fff;
-    h2 {
-      font-size: size(44);
-      font-weight: 600;
-      margin-bottom: size(30);
-      span {
-        display: block;
-        font-size: size(34);
+    display: flex;
+    flex-wrap: wrap;
+    
+    .text_content {
+      padding-right: size(100);
+
+      h2 {
+        font-size: size(44);
+        font-weight: 600;
+        margin-bottom: size(30);
+        span {
+          display: block;
+          font-size: size(34);
+        }
+      }
+
+      p {
+        text-align: justify;
+        font-size: size(24);
+        font-weight: 500;
+        line-height: 1.6;
       }
     }
 
-    p {
-      text-align: justify;
-      font-size: size(24);
+    .materials {
+      margin-top: auto;
+      background-color: #c7ff69;
+      width: 100%;
+      height: size(50);
+
+      color: #000;
+      text-align: center;
+      font-size: size(20);
       font-weight: 500;
-      line-height: 1.6;
+      line-height: 1.7;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
   }
   .logo {
